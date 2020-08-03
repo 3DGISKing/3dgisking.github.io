775d5a257e51e008103cfc844a0f3f59863de293
3dgisking <wugis1219@gmail.com>
2020-08-04 01:51:56 +0800

    update

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    timeline : false,
+    animation : false,
+    baseLayerPicker : false,
+    homeButton : false,
+    sceneModePicker : false,
+    navigationHelpButton : false,
+    geocoder : false
+});
+
